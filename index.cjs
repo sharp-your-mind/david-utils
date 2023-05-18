811e9bac4b6b56293d782fcdbcd4c5fa527a00c2
大卫 <827802450@qq.com>
2023-05-18 21:52:05 +0800

    add cjs

---
+++ index.cjs
@@ -0,0 +1,9 @@
+/**
+ * get add result
+ * @param {nunber} num1 
+ * @param {number} num2 
+ * @returns  numbers
+ */
+const addNumber = (num1, num2) => num1 + num2
+
+module.exports = { addNumber }
